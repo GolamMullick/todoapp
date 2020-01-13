f6c82ab927b351c0e598cf2f1494be76d588e0dd
Fahad <fahadmullick89@gmail.com>
2020-01-13 18:32:54 +0700

    pyramid_3

---
+++ pyramid_3.py
@@ -0,0 +1,15 @@
+rows = input("Enter number of rows ")
+rows = int (rows)
+
+k = 0
+
+for i in range(1, rows+1):
+    for space in range(1, (rows-i)+1):
+        print(end="  ")
+    while k != (2*i-1):
+        print("* ", end="")
+        k = k + 1
+    k = 0
+    print()
+
+
