f016797a6918ae291a0ee2b47943f5153da3bdff
Fahad <fahadmullick89@gmail.com>
2020-01-13 18:42:57 +0700

    exception

---
+++ exception.py
@@ -0,0 +1,3 @@
+"""Finally is implemented regardless of whether the statements in the try block fail or succeed.
+
+Else is executed only if the statements in the try block don't raise an exception"""
